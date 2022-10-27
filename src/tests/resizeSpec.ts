54b1576ec8cf9edf9f59b53aaef59516f753a52c
هانيااحمدسامىعبدالعظ <@DESKTOP-E8F3FRI>
2022-10-28 01:04:38 +0200

    fixed a failed test

--- src/tests/resizeSpec.ts
+++ src/tests/resizeSpec.ts
@@ -4,7 +4,7 @@ import { resize } from '../resize';
 describe('Tests resizing function', () => {
     it('expects the resize function to return the new thumbnail image path', async () => {
         expect(await resize('santamonica', 200, 100)).toEqual(
-            `images/thumbnails/santamonica_200_100.jpg`
+            `images/thumbnails/santamonica-200x100.jpg`
         );
     });
 });
